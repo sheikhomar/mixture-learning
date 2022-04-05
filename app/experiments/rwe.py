35fac059c6258e4f4beb4d901cc3acd342b15f98
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-04-05 08:05:45 +0200

    Add new experiment: `no_bias_single_step_03`.

--- app/experiments/rwe.py
+++ app/experiments/rwe.py
@@ -338,6 +338,21 @@ def no_bias_single_step_02() -> Generator[RandomWalkEmbeddingExperimentParams, N
         )
 
 
+def no_bias_single_step_03() -> Generator[RandomWalkEmbeddingExperimentParams, None, None]:
+    for term in [0.5, 1.0, 1.5]:
+        for n_dim in [300, 200, 100, 50, 10]:
+            params = generate_default_experiment()
+            params["n_dim"] = n_dim
+            params["n_steps"] = 1
+            params["min_distance"] = np.power(n_dim, 1/4) * (term + np.log(n_dim))
+            params["bias_factor"] = 1
+            params["variance"] = 1.0
+            yield from_dict(
+                data_class=RandomWalkEmbeddingExperimentParams, 
+                data=params
+            )
+
+
 def no_bias_multi_step_01() -> Generator[RandomWalkEmbeddingExperimentParams, None, None]:
     for n_steps in [6, 5, 4, 3, 2]:
         for dist_factor in [1, 2, 3, 4, 5, 6]:
@@ -539,6 +554,7 @@ AVAILABLE_EXPERIMENTS = [
     noisy_10_fixed_dist_05,
     no_bias_single_step_01,
     no_bias_single_step_02,
+    no_bias_single_step_03,
     no_bias_multi_step_01,
 ]
 
