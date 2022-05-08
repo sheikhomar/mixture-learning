9d42e1a3c0043ed70b8be2689080fcbd61c5eb11
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-05-08 18:08:41 +0200

    Add new experiments: `e3_k10` and `e3_k20`.

--- app/experiments/pp.py
+++ app/experiments/pp.py
@@ -227,6 +227,34 @@ def e2_steps_20_v2() -> Generator[object, None, None]:
                 yield from_dict(data_class=PlantedPartitionExperimentParams, data=params)
 
 
+def e3_k10() -> Generator[object, None, None]:
+    k = 10
+    for n_steps in [1, 5, 30, 50]:
+        for q in [0.01, 0.10, 0.20, 0.3]:
+            for p_add in [0.2, 0.3, 0.45, 0.6]:
+                p = q + p_add
+                params = generate_default_experiment()
+                params["k"] = k
+                params["n_steps"] = n_steps
+                params["p"] = p
+                params["q"] = q
+                yield from_dict(data_class=PlantedPartitionExperimentParams, data=params)
+
+
+def e3_k20() -> Generator[object, None, None]:
+    k = 20
+    for n_steps in [1, 5, 30, 50]:
+        for q in [0.01, 0.10, 0.20, 0.3]:
+            for p_add in [0.2, 0.3, 0.45, 0.6]:
+                p = q + p_add
+                params = generate_default_experiment()
+                params["k"] = k
+                params["n_steps"] = n_steps
+                params["p"] = p
+                params["q"] = q
+                yield from_dict(data_class=PlantedPartitionExperimentParams, data=params)
+
+
 AVAILABLE_EXPERIMENTS = [
     steps_01,
     steps_02,
@@ -235,6 +263,8 @@ AVAILABLE_EXPERIMENTS = [
     steps_100,
     e2_steps_20,
     e2_steps_20_v2,
+    e3_k10,
+    e3_k20,
 ]
 
 
